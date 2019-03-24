1bff4bc0da7c7f9a5d0dc61fab0f68015c057c78
No-YE <nye7181@gmail.com>
2019-03-24 23:45:37 +0900

    이미 있던 app-chatting 컨테이너와 이미지 삭제

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,12 @@
 cp /home/ubuntu/.env /home/ubuntu/chatting-deploy/.env 
 cd /home/ubuntu/chatting-deploy
 
+if [ "$(docker ps | grep "app-chatting")" != "" ]; then
+  docker rm -f $(docker ps | grep "app-chatting")
+fi
+if [ "$(docker images | grep "app-chatting")" != "" ]; then
+  docker rmi -f $(docker images | grep "app-chatting")
+fi
+
 docker build -t app-chatting .
-docker run -p 3031:3000 -d app-chatting
+docker run --name app-chatting -p 3031:3000 -d app-chatting
