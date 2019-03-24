ebd35538b00711457020741df220acbea0bcd83d
No-YE <nye7181@gmail.com>
2019-03-24 23:10:06 +0900

    docker 빌드 태그 이름 변경

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,5 @@
 cp /home/ubuntu/.env /home/ubuntu/chatting-deploy/.env 
 cd /home/ubuntu/chatting-deploy
 
-docker build -t app .
-docker run -p 3031:3000 -d app
+docker build -t app-chatting .
+docker run -p 3031:3000 -d app-chatting
