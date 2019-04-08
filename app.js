9fa11fd2018e5484ec6993f16baf23bb982b5862
No-YE <nye7181@gmail.com>
2019-04-08 23:33:32 +0900

    redis 클라이언트 추가 & io connection 이벤트 추가

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
 const logger= require('./util/logger');
 require('dotenv').config();
+
+if(process.env.NODE_ENV == 'development')
+  process.env.REDIS_URI = 'localhost'
+
+const redisClient = redis.createClient(6379, process.env.REDIS_URI);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(() => logger.info('connect to mongodb'))
   .catch(e => logger.error(e));
 
-const io = (io) => {
-  
+const io = io => {
+  io.on('connection', socket => {
+    socket.on('sendMessage');
+  });
 };
 
 module.exports = io;
